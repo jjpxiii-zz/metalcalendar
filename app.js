93f1a72abd7668df1068e07a5b2b566f94102802
Your Name <you@example.com>
2018-01-31 23:26:44 +0100

    Get EventsList OK

--- app.js
+++ app.js
@@ -3,5 +3,13 @@ var request = require('request-json');
 var client = request.createClient('http://sueurdemetal.com')
 var data = { region: 12 }
 client.post('/func/funcGetEventRegion.php', data, function (err, res, body) {
-    return console.log(body);
+    console.log(body);
+    res = body.results.collection1
+
+    for (let i = 0; i < res.length; i++) {
+        console.log(new Date(res[i].datetimestamp * 1000))
+        console.log(res[i].ville)
+        res[i].groupes.map(g => console.log(g.NomGroupe))
+    }
+
 });
